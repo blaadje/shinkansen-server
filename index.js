a9f96603e27c1ed51c8d6013f5f95678349487a0
Alexandre Charlot <acharlot91@gmail.com>
2020-02-02 14:10:45 +0100

    integrate socket

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 8080
-const io = require('socket.io')(app)
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+server.listen(port)
 
 io.on('connection', function(socket) {
   console.log('a user connected')
@@ -13,5 +16,3 @@ app.post('/foo', (req, res) => {
   console.log(req.body)
   res.status(200).send('Hello World!')
 })
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
