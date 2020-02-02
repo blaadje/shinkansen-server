4d18e36a1fa879115dccfe981c1ad0ece289629c
Alexandre Charlot <acharlot91@gmail.com>
2020-02-02 12:02:33 +0100

    adding body parser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 8080
 
+app.use(bodyParser.json())
 app.post('/foo', (req, res) => {
   console.log(req.body)
   res.status(200).send('Hello World!')
