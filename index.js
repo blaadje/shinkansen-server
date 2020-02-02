045442d1c0ee0ea610e8e4db73a7f8d657f6dc37
Alexandre Charlot <acharlot91@gmail.com>
2020-02-02 14:04:20 +0100

    adding sockets

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 8080
 
+io.on('connection', function(socket) {
+  console.log('a user connected')
+})
+
 app.use(bodyParser.json())
 app.post('/foo', (req, res) => {
   console.log(req.body)
